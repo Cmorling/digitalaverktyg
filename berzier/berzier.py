1614a56b37babfb10b55a859942d6aa97f779e88
cmorling <caspian.morling@gmail.com>
2023-01-23 22:53:08 +0100

    renaming

--- berzier/berzier.py
+++ berzier/berzier.py
@@ -1,21 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random as rnd
+from nBerzier import Nberzier
 
-def berzier(t, points):
-    scalars = np.array([(1-t)**3, 3*(1-t)*(1-t)*t, 3*(1-t)*t*t, t**3])
-    return np.dot(points.T, scalars)
+
+nb = Nberzier(-15, 15, randomNPoints=6) #Här väljer man antal punkter
+pointBerzier = nb.nBerzier()
 
 ax = plt.figure().add_subplot()
 ax.set_xlim(-15, 15); ax.set_ylim(-15, 15)
-points = np.array([[rnd.uniform(-10.0, 10.0), rnd.uniform(-10.0, 10.0)] for i in range(0,4)]).astype(float)
 
-for idx,point in enumerate(points):
-    ax.plot(*point, 'ro')
-    ax.annotate(['A', 'B', 'C', 'D'][idx], point+.35, fontsize="small")
+for point in nb.getPoints():
+    ax.plot(*point['point'], 'ro')
+    ax.annotate(point['label'], point['point']+.35, fontsize="small")
 
-theta = np.linspace(-15, 15, 1000)
-pointBerzier = berzier(theta, points)
 ax.plot(*pointBerzier, label='Berzier Kurva')
 ax.legend()
 
