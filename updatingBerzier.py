2d74b82a95d185823791a365e98721f02886e92e
cmorling <caspian.morling@gmail.com>
2023-01-23 20:33:08 +0100

    updated names

--- updatingBerzier.py
+++ updatingBerzier.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import random as rnd
 from matplotlib.animation import FuncAnimation
 
-def berzier2(t, points):
+def berzier(t, points):
     scalars = np.array([(1-t)**3, 3*(1-t)*(1-t)*t, 3*(1-t)*t*t, t**3])
     return np.dot(points.T, scalars)
 
@@ -14,8 +14,6 @@ def timerTick_Event(i):
 
     f = plt.gcf()
     ax = f.gca()    
-    #INSERT DELETE FUNCTION HERE
-    print(ax.get_lines())
     
     for item in ax.get_lines():
         item.remove()
@@ -27,13 +25,12 @@ def timerTick_Event(i):
         ax.annotate(['A', 'B', 'C', 'D'][idx], point+.35, fontsize="small")
 
     theta = np.linspace(-10, 10, 1000)
-    pointBerzier = berzier2(theta, points)
+    pointBerzier = berzier(theta, points)
     ax.plot(*pointBerzier, label='Berzier Kurva')
 
 f1 = plt.figure(1)
 ax = f1.add_subplot()
 ax.set_xlim(-15, 15); ax.set_ylim(-15, 15)
-#ax.legend()
 
 timerTickInterval = 2000
 ani = FuncAnimation(f1, timerTick_Event, interval=timerTickInterval)
